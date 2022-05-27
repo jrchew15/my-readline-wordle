556565b324ace37d46b9a1740fb265990c99bfd9
Jason Chew <jrchew15@gmail.com>
2022-05-27 13:10:15 -0700

    fixed yellows issue

--- check-guess.js
+++ check-guess.js
@@ -5,7 +5,7 @@ function checkGuess(solution, guess) {
         if (guess[i] === solution[i]) { feedback[i] = '$'; solutionArr.splice(i, 1) }
     }
     for (let j = 0; j < 5; j++) {
-        if (!feedback[j] && solutionArr.includes(guess[j])) { feedback[j] = '*' }
+        if (!feedback[j] && solutionArr.includes(guess[j])) { feedback[j] = '*'; solutionArr.splice(j, 1) }
         else if (!feedback[j]) { feedback[j] = '_' }
     }
     return feedback;
--- my-wordle.js
+++ my-wordle.js
@@ -7,13 +7,10 @@ let counter = 0;
 let randIndex = Math.floor(allWords.length * Math.random());
 let secretWord = allWords[randIndex].toUpperCase();
 
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-rl.question('Guess a word: ', (guess) => recursiveCheckGuess(secretWord, guess))
 
 function checkGuess(solution, guess) {
     guess = guess.toUpperCase();
@@ -22,7 +19,7 @@ function checkGuess(solution, guess) {
         if (guess[i] === solution[i]) { feedback[i] = '$'; solutionArr.splice(i, 1) }
     }
     for (let j = 0; j < 5; j++) {
-        if (!feedback[j] && solutionArr.includes(guess[j])) { feedback[j] = '*' }
+        if (!feedback[j] && solutionArr.includes(guess[j])) { feedback[j] = '*'; solutionArr.splice(j, 1) }
         else if (!feedback[j]) { feedback[j] = '_' }
     }
     return feedback;
@@ -38,3 +35,5 @@ const recursiveCheckGuess = (solution, guess) => {
     else if (counter >= 6) { console.log('You lose \nTry again!'); rl.close() }
     else { rl.question('Guess a word: ', (guess) => recursiveCheckGuess(solution, guess)) }
 }
+
+rl.question('Guess a word: ', (guess) => recursiveCheckGuess(secretWord, guess))
